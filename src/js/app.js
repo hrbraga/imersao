9ae366b78c74ec4dd135ece86b2cb6d279720509
Hugo Roberto <hugbraga@gmail.com>
2024-09-05 11:58:45 -0300

    Criada função pesquisar no arquivo app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-console.log(ano[35]);
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa");
+
+    let resultados = "";
+    
+    for (let dado of ano) {
+        resultados += `
+        <div class="item-resultado">
+                <p class="ano-prova">${dado.ano}</p>
+                <p class="nome-ciclista">${dado.ciclista}</p>
+                <p class="pais-ciclista">${dado.pais}</p>
+            <div class="prova">
+                <p class="distancia-prova"><i class="fas fa-biking icone"></i> ${dado.distancia}</p>
+                <p class="tempo-prova"><i class="fas fa-stopwatch icone"></i> ${dado.tempo}</p>
+           </div>
+        </div>
+    `
+    }
+    
+    section.innerHTML = resultados
+}
